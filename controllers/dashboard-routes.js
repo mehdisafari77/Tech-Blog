09da718cae284323d8f3028359391e850cec1697
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-28 18:49:30 -0700

    add dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -11,5 +11,32 @@ router.get('/', withAuth, (req, res) => {
         
     })
     const allPosts = postData.map(post => post.get({ plain: true }))
-    res.render('post-admin', { layout: 'dashboard', allPosts })
+    res.render('post-admin', { 
+        layout: 'dashboard', allPosts 
+    })
+    .catch(err => {
+        console.log(err);
+        res.redirect("login");
+      });
 })
+
+router.get("/edit/:id", withAuth, (req, res) => {
+    Post.findByPk(req.params.id)
+      .then(postData => {
+        if (postData) {
+          const post = postData.get({ plain: true });
+          
+          res.render("edit-post", {
+            layout: "dashboard",
+            post
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+  
+module.exports = router;
