1096e8276e0e9bc3dced26052c685a1e1158f1e6
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-28 19:52:48 -0700

    minor fixed on server.js

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
 });
