a25dd09866dfad73ff2e79597e826b01be58d0d5
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-28 19:44:04 -0700

    comment.js content added

---
+++ public/js/comment.js
@@ -0,0 +1,26 @@
+const commentFormHandler = async function(event) {
+    event.preventDefault();
+  
+    const postId = document.querySelector('input[name="post-id"]').value;
+    const postBody = document.querySelector('textarea[name="comment-body"]').value;
+  
+    if (body) {
+      await fetch('/api/comment', {
+        method: 'POST',
+        body: JSON.stringify({
+          postId,
+          postBody
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      document.location.reload();
+    }
+  };
+  
+  document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
