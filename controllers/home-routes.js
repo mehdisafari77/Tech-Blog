5c6772dc842e181f52328bbe691646ea2c8de96c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-28 19:18:02 -0700

    login and signup routes for homepage done

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,3 +40,23 @@ router.get("/post/:id", (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("login");
+  });
+  
+  router.get("/signup", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+  
+    res.render("signup");
+  });
+  
+  module.exports = router;
