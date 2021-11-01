d8f9038b8530c102d8c6790a32077340affc6071
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-01 14:50:28 -0700

    add comments route fix

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,15 +3,13 @@ const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      userId: req.session.userId,
+  Comment.create({ ...req.body, userId: req.session.userId })
+    .then(newComment => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      res.status(500).json(err);
     });
-    res.json(newComment);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
